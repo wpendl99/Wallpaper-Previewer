a4bdab9e73e9dd14980b635eefe21551a375b99c
William Keith Pendleton <wkp23@byu.edu>
2024-12-03 19:40:08 -0700

    Initial Commit

---
+++ Wallpaper Previewer/Wallpaper_PreviewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wallpaper_PreviewerApp.swift
+//  Wallpaper Previewer
+//
+//  Created by William Pendleton on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Wallpaper_PreviewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
